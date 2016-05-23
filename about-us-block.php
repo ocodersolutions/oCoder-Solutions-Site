d7f8329e3eca52da9a4f440baa14a018cd1bd330
Trương Định <truongdinh94@gmail.com>
2016-05-23 15:55:44 +0700

    About us ver 1

--- about-us-block.php
+++ about-us-block.php
@@ -1,4 +1,15 @@
 <!-- about-us-block -->
 <div class="about-us-block">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="uc_about-us-block">
+					<div class="row">
+						<div class="col-md-6"></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 	
 </div>
