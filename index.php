b9dacfe06adc782c469f76876f48a21ad9c69360
lecanhvan <lcv360@gmail.com>
2016-05-23 15:49:56 +0700

    Add all file .less and less.min.js in index.php

--- index.php
+++ index.php
@@ -1,6 +1,20 @@
 <html>
 	<head>
-		
+		<link rel="stylesheet/less" type="text/css" href="menu-block.less" />
+		<link rel="stylesheet/less" type="text/css" href="slidershow-block.less" />
+		<link rel="stylesheet/less" type="text/css" href="our-service-block.less" />
+		<link rel="stylesheet/less" type="text/css" href="about-us-block.less" />
+		<link rel="stylesheet/less" type="text/css" href="member-team-block.less" />
+		<link rel="stylesheet/less" type="text/css" href="feature-project-block.less" />
+		<link rel="stylesheet/less" type="text/css" href="subscribe-block.less" />
+		<link rel="stylesheet/less" type="text/css" href="portfolio-block.less" />
+		<link rel="stylesheet/less" type="text/css" href="testimonial-block.less" />
+		<link rel="stylesheet/less" type="text/css" href="our-pricing-block.less" />
+		<link rel="stylesheet/less" type="text/css" href="partnerslide-block.less" />
+		<link rel="stylesheet/less" type="text/css" href="blog-block.less" />
+		<link rel="stylesheet/less" type="text/css" href="contact-block.less" />
+		<link rel="stylesheet/less" type="text/css" href="footer-block.less" />
+		<script src="//cdnjs.cloudflare.com/ajax/libs/less.js/2.6.1/less.min.js"></script>
 	</head>
 	<body>
 		<div class="wrapper">
