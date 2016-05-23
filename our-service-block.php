62aab5e89b5d0513e9fd4d9e3f51c2b3bb3f1763
lecanhvan <lcv360@gmail.com>
2016-05-23 16:28:35 +0700

    Add bootstrap in index.php

--- our-service-block.php
+++ our-service-block.php
@@ -1,4 +1,8 @@
 <!-- our-service-block -->
 <div class="our-service-block">
-	<div></div>
+	<div class="container">
+		<div class="row"></div>
+		<div class="row"></div>
+		<div class="row"></div>
+	</div>
 </div>
