210f28e492cb92e9632ab6fd9f23a0acd692e4bd
lecanhvan <lcv360@gmail.com>
2016-05-24 16:52:46 +0700

    our-pricing-block & our-service-block (almost done, wait js button change)

--- our-service-block.php
+++ our-service-block.php
@@ -3,7 +3,7 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-md-6 col-md-offset-3 text-center">
-				<h2>OUR SERVICES</h2>
+				<h2 class="tittle">OUR SERVICES</h2>
 				<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper</p>
 			</div>
 		</div>
