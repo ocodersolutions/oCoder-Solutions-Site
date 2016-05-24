3817aedec174d2bdd133b61ceab4838a18c37e44
lecanhvan <lcv360@gmail.com>
2016-05-24 15:28:27 +0700

    our-pricing-block (unfinish)

--- our-pricing-block.php
+++ our-pricing-block.php
@@ -1,4 +1,109 @@
 <!-- our-pricing-block-->
 <div class="our-pricing-block">
-	
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6 col-md-offset-3 text-center">
+				<h2>our pricing</h2>
+				<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper</p>
+			</div>
+		</div>
+		<div class="row text-center">
+			<div class="col-md-3 col-sm-6">
+				<div class="item">
+					<div class="top">
+						<div class="title"><span>Basic Plant</span></div>
+						<div class="price">
+							<span class="sign">$</span>
+							<span class="amount">10</span>
+						</div>
+					</div>
+					<div class="body">
+						<ul>
+							<li>5 Projects</li>
+							<li>10GB Disk Space</li>
+							<li>100 Images</li>
+							<li>2 E-mail Adresses</li>
+							<li>1 Domain</li>
+							<li>5 GB Hosting</li>
+						</ul>
+					</div>
+					<div class="bottom">
+						<a class="button-effecs">buy now</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-3 col-sm-6">
+				<div class="item active">
+					<div class="top">
+						<div class="title"><span>Standard Plan</span></div>
+						<div class="price">
+							<span class="sign">$</span>
+							<span class="amount">50</span>
+						</div>
+					</div>
+					<div class="body">
+						<ul>
+							<li>5 Projects</li>
+							<li>10GB Disk Space</li>
+							<li>100 Images</li>
+							<li>2 E-mail Adresses</li>
+							<li>1 Domain</li>
+							<li>5 GB Hosting</li>
+						</ul>
+					</div>
+					<div class="bottom">
+						<a class="button-effecs">buy now</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-3 col-sm-6">
+				<div class="item">
+					<div class="top">
+						<div class="title"><span>Business Plan</span></div>
+						<div class="price">
+							<span class="sign">$</span>
+							<span class="amount">80</span>
+						</div>
+					</div>
+					<div class="body">
+						<ul>
+							<li>5 Projects</li>
+							<li>10GB Disk Space</li>
+							<li>100 Images</li>
+							<li>2 E-mail Adresses</li>
+							<li>1 Domain</li>
+							<li>5 GB Hosting</li>
+						</ul>
+					</div>
+					<div class="bottom">
+						<a class="button-effecs">buy now</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-3 col-sm-6">
+				<div class="item">
+					<div class="top">
+						<div class="title"><span>Advanced Plan</span></div>
+						<div class="price">
+							<span class="sign">$</span>
+							<span class="amount">100</span>
+						</div>
+					</div>
+					<div class="body">
+						<ul>
+							<li>5 Projects</li>
+							<li>10GB Disk Space</li>
+							<li>100 Images</li>
+							<li>2 E-mail Adresses</li>
+							<li>1 Domain</li>
+							<li>5 GB Hosting</li>
+						</ul>
+					</div>
+					<div class="bottom">
+						<a class="button-effecs">buy now</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
