3a86a61a8869b36b3c2b61db5427aa0b86ff5848
Trương Định <truongdinh94@gmail.com>
2016-05-25 09:22:43 +0700

    Add progees bar in file base and push about task

--- blog-block.php
+++ blog-block.php
@@ -1,4 +1,56 @@
 <!-- blog-block -->
 <div class="blog-block">
-	
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6 col-md-offset-3 text-center">
+				<h2 class="tittle">Blog</h2>
+				<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-4 col-sm-6 blog-item">
+				<div class="main-card default-shadow-container">
+					<div class="main-blog-item-title">
+						<div class="meta-data">
+							<div class="date">
+								<a href="#">
+									<i class="fa fa-lg fa-calendar">
+										<span class="day">29</span>
+										<span class="month">Jan</span>
+									</i>
+								</a>
+							</div>
+							<div class="author-img">
+								<a href="#">
+									<img src="assets/img/author.jpg" alt="">
+								</a>
+							</div>
+						</div>
+						<div class="blog-item-img">
+							<a href="#">
+								<img src="assets/img/portfolio-block-04.jpg" alt="">
+							</a>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id eius aspernatur eaque exercitationem omnis maxime magni ducimus quo</p>
+						</div>
+					</div>
+					<div class="blog-supporting-text">
+						<h2 class="blog-item-title">
+							<a href="#">Lorem ipsum sit amet dolor consectetur</a>
+						</h2>
+					</div>
+					<div class="action-bar">
+						<a href="#" class="button-effecs ripple">
+							Read More
+						</a>
+						
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12 text-center">
+				<a class="button-effecs ripple discover-more" href="#">discover more</a>
+			</div>
+		</div>
+	</div>
 </div>
