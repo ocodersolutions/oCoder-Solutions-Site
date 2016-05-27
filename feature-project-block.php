98a55c1fec88cc276abd51a03255fbb68873c9eb
lecanhvan <lcv360@gmail.com>
2016-05-27 08:28:50 +0700

    feature-project-block (continue...)

--- feature-project-block.php
+++ feature-project-block.php
@@ -23,7 +23,13 @@
 			</div>
 			<div class="col-md-4">
 				<div class="item">
-					<div class="item_image"><img src="assets/img/portfolio-block-05.jpg" /></div>
+					<div class="item_image">
+						<div class="owl-carousel">
+							<div><img src="assets/img/portfolio-block-05.jpg" /></div>
+							<div><img src="assets/img/portfolio-block-02.jpg" /></div>
+							<div><img src="assets/img/portfolio-block-01.jpg" /></div>
+						</div>
+					</div>
 					<div class="item_info">
 						<h2><a href="#">Lorem ipsum dolor sit amet elit consectetuer</a></h2>
 						<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
@@ -54,3 +60,14 @@
 		</div>
 	</div>
 </div>
+<script>
+	$(document).ready(function(){
+		$("#brand").owlCarousel({
+			//pagination: false,
+			autoPlay: true,
+			itemsCustom: [
+				[0, 1]
+			],
+		});
+	});
+</script>
