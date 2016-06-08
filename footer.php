93e3ebbc7f4a70b71301901a3df408bf42f441fb
lecanhvan <lcv360@gmail.com>
2016-06-08 15:36:29 +0700

    add footer.php

--- footer.php
+++ footer.php
@@ -4,25 +4,68 @@
 		<div class="row">
 			<div class="col-md-6">
 				<div class="row">
-					<div class="col-md-6 col-sm-6">
+					<div class="col-sm-6">
 						<h2 class="titleline">Latest news</h2>
 						<div class="news_item">
 							<div class="left"><img src="assets/img/latesnew.jpg" alt="" /></div>
 							<div class="right">
 								<a href="#">Lorem Ipsum Sit Amut</a>
 								<p>You shouldn't have to take our word for it. You sdfd read what i You shouldn't have to take our</p>
-								<span>Published ON: <span>10 dec</span></span>
+								<span>Published ON: <i>10 Dec</i></span>
+							</div>
+						</div>
+						<div class="news_item">
+							<div class="left"><img src="assets/img/latesnew.jpg" alt="" /></div>
+							<div class="right">
+								<a href="#">Lorem Ipsum Sit Amut</a>
+								<p>You shouldn't have to take our word for it. You sdfd read what i You shouldn't have to take our</p>
+								<span>Published ON: <i>10 Dec</i></span>
 							</div>
 						</div>
 					</div>
-					<!--?-->
-					<div class="col-md-6 col-sm-6"></div>
+					<div class="col-sm-6">
+						<h2 class="titleline">Latest tweets</h2>
+						<div class="tweets_allitem">
+							<ul>
+								<li><p>Check out <a href="#">#PriceX</a> is a <a href="#">#Material_Design</a> <a href="#">#Pricing_Table</a> Set including Seven Exclusive material design <a href="#">#pricing_tables</a> <a href="#">http://goo.gl/namRZO</a></p></li>
+								<li><p>Check out <a href="#">#MatRoz</a> <a href="#">#Material_Design</a> Agency and <a href="#">#Business_HTML5_Template</a> on <a href="#">#EnvatoMarket</a> by @ThemeLooks <a href="#">#themeforest</a> <a href="#">http://goo.gl/mmCUaO</a></p></li>
+							</ul>
+						</div>
+					</div>
 				</div>
 			</div>
-			<!--?-->
-			<div class="col-md-6"></div>
+			<div class="col-md-6">
+				<div class="row">
+					<div class="col-sm-6">
+						<h2 class="titleline">Tags</h2>
+						<ul class="tags_allitem">
+							<li><a href="#">Business</a></li>
+							<li><a href="#">Agency</a></li>
+							<li><a href="#">Creative</a></li>
+							<li><a href="#">Portfolio</a></li>
+							<li><a href="#">Coporate</a></li>
+							<li><a href="#">Website</a></li>
+							<li><a href="#">Business Website</a></li>
+							<li><a href="#">Coporate Website</a></li>
+						</ul>
+					</div>
+					<div class="col-sm-6">
+						<h2 class="titleline">Links</h2>
+						<ul class="links_allitem">
+							<li><a href="#">lorem ipsum sit amut</a></li>
+							<li><a href="#">lorem ipsum sit amut</a></li>
+							<li><a href="#">lorem ipsum sit amut</a></li>
+							<li><a href="#">lorem ipsum sit amut</a></li>
+							<li><a href="#">lorem ipsum sit amut</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
 </div>
 <div class="copyright">
+	<div class="container">
+		<p>Copyright 2016 &copy; <a href="#">BizLinks</a>. All right Reserved.</p>
+	</div>
 </div>
