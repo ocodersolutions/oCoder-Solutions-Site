9a896964a0d32d298e7ceeeaabb058df1b39cbe1
lecanhvan <lcv360@gmail.com>
2016-06-10 15:07:16 +0700

    fix slidershow-block (center content in slider)

--- slidershow-block.php
+++ slidershow-block.php
@@ -1,113 +1,96 @@
 <!-- slidershow-block -->
 <div id="header" class="slidershow-block">
-	<div class="header-items owl-carousel owl-theme" id="slideshowHeader">
-		<!--item 1-->
-		<div class="slider_item">
-			<div class="slider_details">
-				<div class="slider_image">
-					<div>
-						<img alt="client" class="img-responsive" src="assets/img/header-slider-1.jpg"/>
-					</div>
-					<!-- Header Slider Content Start -->
-					<div class="header-slider-content">
-						<div class="center-header-slider-content">
-							<div class="container">
-								<div class="row">
-									<div class="col-md-6">
-										<!-- Slider Image Start -->
-										<div class="slider-1-img">
-											<img src="assets/img/laptop.png" alt="" class="img-responsive img-1">
-											<img src="assets/img/desk.jpg" alt="" class="img-responsive img-2">
-										</div>
-										<!-- Slider Image End -->
-									</div>
-									<div class="col-md-6 slider-1-content-c">
-										<!-- Slider Content Start -->
-										<div class="slider-1-content">
-											<h2 class="mdl-typography--display-2 mdl-typography--text-capitalize">lorem ipsum dolor</h2>
-											<h4 class="mdl-typography--text-capitalize">lorem ipsum dolor sit amet, consectetur adipisicing elit.</h4>
-											<a href="#" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">buy now</a>
-										</div>
-										<!-- Slider Content End -->
-									</div>
+	<div id="header_slide" class="header-items owl-carousel owl-theme">
+		<!-- start of item 1 -->
+		<div class="header-item img-bg-1">
+			<div class="header-slider-content">
+				<div class="center-header-slider-content">
+					<div class="container">
+						<div class="row">
+							<div class="col-md-6 col-edit">
+								<div class="slider-1-img">
+									<img class="img-responsive img-1" src="assets/img/laptop.png" />
+									<img class="img-responsive img-2" src="assets/img/desk.jpg" />
+								</div>
+							</div>
+							<div class="col-md-6 col-edit slider-1-content-c">
+								<div class="slider-content slider-move-1">
+									<h2>Lorem ipsum dolor</h2>
+									<h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h4>
+									<a href="#">Buy now</a>
 								</div>
 							</div>
 						</div>
 					</div>
-					<!-- Header Slider Content End -->
 				</div>
 			</div>
 		</div>
-		<!-- end item 1-->
-		<!--item 1-->
-		<div class="slider_item">
-			<div class="slider_details">
-				<div class="slider_image">
-					<div>
-						<img alt="client" class="img-responsive" src="assets/img/header-slider-2.jpg"/>
-					</div>
-					<!-- Header Slider Content Start -->
-					<div class="header-slider-content">
-						<div class="center-header-slider-content">
-							<div class="container">
-								<div class="row">
-									<div class="col-md-6 slider-1-content-c">
-										<!-- Slider Content Start -->
-										<div class="slider-1-content">
-											<h2 class="mdl-typography--display-2 mdl-typography--text-capitalize">lorem ipsum dolor</h2>
-											<h4 class="mdl-typography--text-capitalize">lorem ipsum dolor sit amet, consectetur adipisicing elit.</h4>
-											<a href="#" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">buy now</a>
-										</div>
-										<!-- Slider Content End -->
-									</div>
+		<!-- end of item 1 -->
+
+		<!-- start of item 2 -->
+		<div class="header-item img-bg-2">
+			<div class="header-slider-content">
+				<div class="center-header-slider-content">
+					<div class="container">
+						<div class="row">
+							<div class="col-md-12 text-center">
+								<div class="slider-content slider-move-2">
+									<h2>Lorem ipsum dolor amet consectetur</h2>
+									<h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h4>
+									<a href="#">Buy now</a>
 								</div>
 							</div>
 						</div>
 					</div>
-					<!-- Header Slider Content End -->
 				</div>
 			</div>
 		</div>
-		<!-- end item 1-->
-		<!--item 1-->
-		<div class="slider_item">
-			<div class="slider_details">
-				<div class="slider_image">
-					<div>
-						<img alt="client" class="img-responsive" src="assets/img/header-slider-1.jpg"/>
+		<!-- end of item 2 -->
+
+		<!-- start of item 3 -->
+		<div class="header-item img-bg-2">
+			<div class="header-slider-content">
+				<div class="center-header-slider-content">
+					<div class="container">
+						<div class="row">
+							<div class="col-md-12 text-center">
+								<div class="slider-content slider-move-3">
+									<h2>Lorem ipsum dolor amet consectetur</h2>
+									<h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h4>
+									<a href="#">Buy now</a>
+								</div>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
 		</div>
-		<!-- end item 1-->
+		<!-- end of item 3 -->
 	</div>
 </div>
 <script type="text/javascript">
 	$(document).ready(function() {
-		$('#slideshowHeader').owlCarousel({
+		$("#header_slide").owlCarousel({
 			itemsCustom: [
-				[0, 1],
-				[1600, 1]
+				[0, 1]
 			],
-			afterAction: function(el) {
-				//remove class active
-				this
-					.$owlItems
-					.removeClass('active')
-
-				//add class active
-				this
-					.$owlItems //owl internal $ object containing items
-					.eq(this.currentItem)
-					.addClass('active')
-
-			},
 			navigation: false,
 			slideSpeed: 300,
 			paginationSpeed: 400,
 			pagination: true,
 			rewindSpeed: 500,
-			autoPlay: true
+			autoPlay: true,
+			afterAction: function(el) {
+				//remove class active
+				this
+					.$owlItems
+					.removeClass("active")
+				//add class active
+				this
+					.$owlItems //owl internal $ object containing items
+					.eq(this.currentItem)
+					.addClass("active")
+			}
 		});
 	});
 </script>
