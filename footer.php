b831b955e9e83c8eae1499fb9fd090ebcf708240
Phan Văn Huy Hoàng <pvhuyhoang166@gmail.com>
2016-06-11 09:39:45 +0700

    cm

--- footer.php
+++ footer.php
@@ -1,4 +1,94 @@
 <!-- footer -->
 <div class="footer">
-	
+	<div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="row">
+                    <!-- Latest News Widget Start -->
+                    <div class="col-md-6 col-sm-6 latest-news-widget">
+                        <h2 class="footer-title">latest news</h2>
+                        <div class="footer-thumb">
+                            <div class="row-holder">
+                                <div class="left-col">
+                                    <img src="assets/img/recent-item-1.jpg" alt="">
+                                </div>
+                                <div class="right-col">
+                                    <a class="footer-thumb-link" href="#">
+                                        Lorem Ipsum sit amut
+                                    </a>
+                                    <p class="footer-thumb-text">
+                                        You shouldn't have to take our word for it. You sdfd read what i You shouldn't have to take our
+                                    </p>
+                                    <span class="footer-thumb-date">Published ON: 
+                                        <span class="footer-thumb-text-italic">10 dec</span>
+                                    </span>
+                                </div>
+                            </div>
+                            <div class="row-holder">
+                                <div class="left-col">
+                                    <img src="assets/img/recent-item-1.jpg" alt="">
+                                </div>
+                                <div class="right-col">
+                                    <a class="footer-thumb-link" href="#">
+                                        Lorem Ipsum sit amut
+                                    </a>
+                                    <p class="footer-thumb-text">
+                                        You shouldn't have to take our word for it. You sdfd read what i You shouldn't have to take our  
+                                    </p>
+                                    <span class="footer-thumb-date">Published ON: 
+                                        <span class="footer-thumb-text-italic">10 dec</span>
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Latest News Widget End -->
+                    <!-- Latest Tweets Widget Start -->
+                    <div class="col-md-6 col-sm-6 latest-tweets-widget">
+                        <h2 class="footer-title">latest tweets</h2>
+                        <div id="tweets">
+	                        <ul>
+	                        <li><p class="tweet">Check out <a href="https://twitter.com/hashtag/MatRoz?src=hash" data-scribe="element:hashtag" target="_blank"><span>#</span><span>MatRoz</span></a> | <a href="https://twitter.com/hashtag/Material_Design?src=hash" data-scribe="element:hashtag" target="_blank"><span>#</span><span>Material_Design</span></a> Agency and <a href="https://twitter.com/hashtag/Business_HTML_Template?src=hash" data-scribe="element:hashtag" target="_blank"><span>#</span><span>Business_HTML_Template</span></a> on <a href="https://twitter.com/hashtag/EnvatoMarket?src=hash" data-scribe="element:hashtag" target="_blank"><span>#</span><span>EnvatoMarket</span></a> by <a href="https://twitter.com/ThemeLooks" data-mentioned-user-id="4711923134" data-scribe="element:mention" target="_blank"><span>@</span><span>ThemeLooks</span></a> <a href="https://twitter.com/hashtag/themeforest?src=hash" data-scribe="element:hashtag" target="_blank"><span>#</span><span>themeforest</span></a> <a href="https://t.co/nWQSFEYcKI" data-expanded-url="http://goo.gl/mmCUaO" target="_blank" title="http://goo.gl/mmCUaO" data-scribe="element:url"><span>http://</span>goo.gl/mmCUaO<span>&nbsp;</span></a></p></li><li><p class="tweet">lilyHost is a <a href="https://twitter.com/hashtag/Responsive?src=hash" data-scribe="element:hashtag" target="_blank"><span>#</span><span>Responsive</span></a> <a href="https://twitter.com/hashtag/HTML5?src=hash" data-scribe="element:hashtag" target="_blank"><span>#</span><span>HTML5</span></a> <a href="https://twitter.com/hashtag/Hosting?src=hash" data-scribe="element:hashtag" target="_blank"><span>#</span><span>Hosting</span></a> <a href="https://twitter.com/hashtag/WHMCS_Template?src=hash" data-scribe="element:hashtag" target="_blank"><span>#</span><span>WHMCS_Template</span></a> designed <a href="https://twitter.com/hashtag/Hosting_Business?src=hash" data-scribe="element:hashtag" target="_blank"><span>#</span><span>Hosting_Business</span></a>. <a href="https://t.co/HLZiKOjZxV" data-expanded-url="https://www.mojomarketplace.com/item/lilyhost-html5-hosting-template" target="_blank" title="https://www.mojomarketplace.com/item/lilyhost-html5-hosting-template" data-scribe="element:url"><span>https://www.</span>mojomarketplace.com/item/lilyhost-<span>html5-hosting-template&nbsp;</span>…</a> <a href="https://twitter.com/hashtag/MOJO?src=hash" data-scribe="element:hashtag" target="_blank"><span>#</span><span>MOJO</span></a> via <a href="https://twitter.com/mojomarketplace" data-mentioned-user-id="103115437" data-scribe="element:mention" target="_blank"><span>@</span><span>mojomarketplace</span></a></p></li>
+	                        </ul>
+                        </div>
+                    </div>
+                    <!-- Latest Tweets Widget End -->
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="row">
+                    <!-- Tags Widget Start -->
+                    <div class="col-md-6 col-sm-6 tags-widget">
+                        <h2 class="footer-title">Tags</h2>
+                        <ul>
+                            <li><a href="#" class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--raised mdl-button--accent" data-upgraded=",MaterialButton,MaterialRipple">business<span class="mdl-button__ripple-container"><span class="mdl-ripple"></span></span></a></li>
+                            <li><a href="#" class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--raised mdl-button--accent" data-upgraded=",MaterialButton,MaterialRipple">agency<span class="mdl-button__ripple-container"><span class="mdl-ripple"></span></span></a></li>
+                            <li><a href="#" class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--raised mdl-button--accent" data-upgraded=",MaterialButton,MaterialRipple">creative<span class="mdl-button__ripple-container"><span class="mdl-ripple"></span></span></a></li>
+                            <li><a href="#" class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--raised mdl-button--accent" data-upgraded=",MaterialButton,MaterialRipple">portfolio<span class="mdl-button__ripple-container"><span class="mdl-ripple"></span></span></a></li>
+                            <li><a href="#" class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--raised mdl-button--accent" data-upgraded=",MaterialButton,MaterialRipple">coporate<span class="mdl-button__ripple-container"><span class="mdl-ripple"></span></span></a></li>
+                            <li><a href="#" class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--raised mdl-button--accent" data-upgraded=",MaterialButton,MaterialRipple">website<span class="mdl-button__ripple-container"><span class="mdl-ripple"></span></span></a></li>
+                            <li><a href="#" class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--raised mdl-button--accent" data-upgraded=",MaterialButton,MaterialRipple">business website<span class="mdl-button__ripple-container"><span class="mdl-ripple"></span></span></a></li>
+                            <li><a href="#" class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--raised mdl-button--accent" data-upgraded=",MaterialButton,MaterialRipple">coporate website<span class="mdl-button__ripple-container"><span class="mdl-ripple"></span></span></a></li>
+                        </ul>
+                    </div>
+                    <!-- Tags Widget End -->
+                    <!-- Links Widget Start -->
+                    <div class="col-sm-6">
+                        <section class="links-widget">
+                            <h2 class="footer-title">links</h2>
+                            <ul>
+                                <li><a href="#">lorem ipsum sit amut</a></li>
+                                <li><a href="#">lorem ipsum sit amut</a></li>
+                                <li><a href="#">lorem ipsum sit amut</a></li>
+                                <li><a href="#">lorem ipsum sit amut</a></li>
+                                <li><a href="#">lorem ipsum sit amut</a></li>
+                            </ul>
+                        </section>
+                    </div>
+                    <!-- Links Widget End -->
+                </div>
+            </div>
+        </div>
+    </div>
+    
 </div>
